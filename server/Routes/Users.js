b17de830c0ddfe03fc06e24d361290db31f9a2c5
Panagiotis Malliotas <pmalliotas@gmail.com>
2021-04-26 19:59:18 +0300

    v0.1

---
+++ server/Routes/Users.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+
+
+router.get('/', (req, res, next) => {
+    
+})
+
+module.exports = router;
--- server/app.js
+++ server/app.js
@@ -2,11 +2,14 @@ const express = require('express');
 const cors = require("cors");
 require("dotenv").config();
 
+const Users = require("./Routes/Users");
+
 const app = express();
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cors({ origin: "*" }));
 
+app.use("/users", Users);
 
 
 app.listen(process.env.PORT, () => {
